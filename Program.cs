0c3a9c361af76d6f34740a8d382361ac939039dd
Miguel Montoya <mamt222007@gmail.com>
2026-03-14 16:59:17 -0500

    chore: Creación de las ramas develop y feature/project-bootstrap con su mensaje de bienvenida

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("¡Bienvenido a Vivi Librería!");
+        Console.WriteLine("Tu lugar favorito para libros y conocimiento.");
+    }
+}
